d879c69ecc6c0425541b83c3bdfed2bc7462e826
Dwayne Crooks <me@dwaynecrooks.com>
2020-09-08 16:58:09 -0400

    Add an evaluator to determine when a grid is won, squashed or neither

--- src/lib.rs
+++ src/lib.rs
@@ -46,14 +46,10 @@ impl Grid {
             .collect()
     }
 
-    pub fn cells(&self) -> Vec<(Position, &Cell)> {
+    pub fn cells(&self) -> Vec<&Cell> {
         // FIXME: Return an iterator.
 
-        self.cells
-            .iter()
-            .enumerate()
-            .map(|(i, cell)| (Self::pos(i), cell))
-            .collect()
+        self.cells.iter().collect()
     }
 
     fn index((r, c): Position) -> usize {
@@ -63,6 +59,45 @@ impl Grid {
     fn pos(index: usize) -> Position {
         (index / SIZE, index % SIZE)
     }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Outcome {
+    Win,
+    Squash
+}
+
+fn evaluate(grid: &Grid, mark: Mark) -> Option<Outcome> {
+    let cells = grid.cells();
+
+    if is_win(&cells, mark) {
+        Some(Outcome::Win)
+    } else if is_squash(&cells) {
+        Some(Outcome::Squash)
+    } else {
+        None
+    }
+}
+
+fn is_win(cells: &[&Cell], mark: Mark) -> bool {
+    let t = Some(mark);
+
+    ARRANGEMENTS.iter().any(|&(i, j, k)| (cells[i], cells[j], cells[k]) == (&t, &t, &t))
+}
+
+const ARRANGEMENTS: [(usize, usize, usize); 8] = [
+    (0, 1, 2),
+    (3, 4, 5),
+    (6, 7, 8),
+    (0, 3, 6),
+    (1, 4, 7),
+    (2, 5, 8),
+    (0, 4, 8),
+    (2, 4, 6)
+];
+
+fn is_squash(cells: &[&Cell]) -> bool {
+    cells.iter().all(|cell| cell.is_some())
 }
 
 #[cfg(test)]
@@ -97,9 +132,47 @@ mod tests {
         ]);
 
         assert_eq!(grid.cells(), vec![
-            ((0, 0), &Some(Mark::X)), ((0, 1), &None), ((0, 2), &None),
-            ((1, 0), &None), ((1, 1), &Some(Mark::O)), ((1, 2), &None),
-            ((2, 0), &None), ((2, 1), &None), ((2, 2), &None)
+            &Some(Mark::X), &None, &None,
+            &None, &Some(Mark::O), &None,
+            &None, &None, &None
         ]);
+    }
+
+    #[test]
+    fn evaluate_on_an_empty_grid_returns_none() {
+        let grid = Grid::new();
+
+        assert!(evaluate(&grid, Mark::X).is_none());
+        assert!(evaluate(&grid, Mark::O).is_none());
+    }
+
+    #[test]
+    fn evaluate_detects_a_win() {
+        let mut grid = Grid::new();
+
+        grid.set((0, 0), Mark::X);
+        grid.set((1, 0), Mark::O);
+        grid.set((0, 1), Mark::X);
+        grid.set((1, 1), Mark::O);
+        grid.set((0, 2), Mark::X);
+
+        assert_eq!(evaluate(&grid, Mark::X), Some(Outcome::Win));
+    }
+
+    #[test]
+    fn evaluate_detects_a_squash() {
+        let mut grid = Grid::new();
+
+        grid.set((0, 0), Mark::X);
+        grid.set((1, 1), Mark::O);
+        grid.set((0, 1), Mark::X);
+        grid.set((0, 2), Mark::O);
+        grid.set((2, 0), Mark::X);
+        grid.set((1, 0), Mark::O);
+        grid.set((1, 2), Mark::X);
+        grid.set((2, 2), Mark::O);
+        grid.set((2, 1), Mark::X);
+
+        assert_eq!(evaluate(&grid, Mark::X), Some(Outcome::Squash));
     }
 }
